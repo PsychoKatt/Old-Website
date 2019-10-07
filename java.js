20736c7f92c37608b6be88b84f6b1f2965293dcc
Glitch (thimble-import) <none>
2019-10-07 21:55:05 +0000

    💄🛤 Checkpoint

---
+++ java.js
@@ -0,0 +1,13 @@
+// Select an element with id="button"
+var element = document.querySelector('#button');
+
+element.addEventListener("click", function() {
+  console.log("Click!");
+});
+
+// boiiiiiiiiii other javascrip
+
+// Select the element with id='alert'
+var element = document.querySelector('#alert');
+
+element.style.background = "OrangeRed";
